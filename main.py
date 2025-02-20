4e857644a63e20cb5129053cbd4f44cf470407fe
VM_N <necvladm@gmail.com>
2025-02-20 19:34:36 +0200

    add main read book

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main() -> str:
+    book_path = "books/frankenstein.txt"
+    book_text = get_text(book_path)
+    print(book_text)
+
+
+def get_text(book):
+    with open(book) as f:
+        file_contents = f.read()
+    return file_contents
+
+    
+main()
