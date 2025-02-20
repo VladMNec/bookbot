7b3d2cd45bac152948502ece4d2f2caa1ff8ba51
VM_N <necvladm@gmail.com>
2025-02-20 21:27:50 +0200

    add word count character count

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 def main() -> str:
     book_path = "books/frankenstein.txt"
     book_text = get_text(book_path)
+    book_word_count = count_words(book_text)
+    book_character_count = count_characters(book_text)
     print(book_text)
+    print(f"{book_word_count} words found in the document")
+    print(book_character_count)
+
+
+def count_words(book_text):
+    return len(book_text.split())
+
+
+def count_characters(book_text):
+    count_dict = {}
+    book_text_lower = book_text.lower()
+    for letter in book_text_lower:
+        if letter in count_dict:
+            count_dict[letter] += 1
+        else:
+            count_dict[letter] = 1
+    return count_dict
 
 
 def get_text(book):
@@ -9,5 +28,5 @@ def get_text(book):
         file_contents = f.read()
     return file_contents
 
-    
+
 main()
